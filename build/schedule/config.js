07c062a81ecaaabdcf802cf16a01cd922b8cf7ee
舒克 <shuke.cl@taobao.com>
2014-02-26 16:57:27 +0800

    Clam Build: 0.1.9 - 2014-02-26 04:57:27

--- build/schedule/config.js
+++ build/schedule/config.js
@@ -5,7 +5,7 @@ tools/src/schedule/config
 
 */
 /**
- * http://g.tbcdn.cn/trip/trip-home/0.1.4/config.js
+ * http://g.tbcdn.cn/trip/trip-home/0.1.9/config.js
  */
 (function(){
     KISSY.config('tag', null); //去除?t时间戳
